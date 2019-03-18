d6e8bf78c85ea0bc9b65e2c22a4d98aa5eafc9c0
Kakus <kkarpiak94@gmail.com>
2019-03-19 00:10:11 +0100

    adding a SWV console

--- Magisterka_normal/Src/syscalls.c
+++ Magisterka_normal/Src/syscalls.c
@@ -52,6 +52,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <sys/times.h>
+#include "stm32f4xx.h"
 
 
 /* Variables */
@@ -106,7 +107,7 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
 	for (DataIdx = 0; DataIdx < len; DataIdx++)
 	{
-		__io_putchar(*ptr++);
+		ITM_SendChar(*ptr++);
 	}
 	return len;
 }
